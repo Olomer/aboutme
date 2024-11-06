63f007998cd054bf6cab17b463b7751b5506bd77
Marek Malina <malina.marek@hotmail.com>
2024-11-06 09:35:27 +0100

    Updated the deploy config.

--- next.config.mjs
+++ next.config.mjs
@@ -12,4 +12,10 @@ export default withBundleAnalyzer({
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  output: "export",
+  basePath: "/aboutme",
+  images: {
+    unoptimized: true,
+  },
 });
+
