d3697d00b550f9ce1fea33d3bd767ae952bf85a1
Marek Malina <malina.marek@hotmail.com>
2024-11-10 13:21:10 +0100

    Updated styles for the main page.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Image, Text, Group, Title, Grid, TypographyStylesProvider } from "@mantine/core";
+import { Image, Text, Group, Title, Grid, TypographyStylesProvider, Flex, Center } from "@mantine/core";
 import about from "../data/about/aboutApiService";
 import {
   useTranslation,
@@ -13,18 +13,20 @@ export default function HomePage() {
   const data = about.getAboutData();
 
   return (
-    <Group grow >
-      <Image radius="100%" h={200} w="auto" fit="contain" src="images/profile.jpg" mx={100} />
-      <Grid pt="md">
-        <Grid.Col span={12}><Title order={1}>{data.name}</Title></Grid.Col>
-        <Grid.Col span={12}>
-          <TypographyStylesProvider>
-            <div
-              dangerouslySetInnerHTML={{ __html: data.description }}
-            />
-          </TypographyStylesProvider>
-        </Grid.Col>
-      </Grid>
-    </Group>
+    <Grid pt="md" mx={"10%"} align="center">
+      <Grid.Col span={{ xs: 12, md: 3.3 }}>
+        <Center>
+          <Image radius="100%" mah="200" w="auto" fit="contain" src="images/profile.jpg" mr={{ md: 10 }} />
+        </Center>
+      </Grid.Col>
+      <Grid.Col span={{ xs: 12, md: 8.7 }}>
+        <Title order={1}>{data.name}</Title>
+        <TypographyStylesProvider>
+          <div
+            dangerouslySetInnerHTML={{ __html: data.description }}
+          />
+        </TypographyStylesProvider>
+      </Grid.Col>
+    </Grid>
   );
 }
